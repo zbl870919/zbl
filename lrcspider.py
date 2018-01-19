a7529494dee24897954f0356739941479929b827
zbl870919 <zbl870919@126.com>
2018-01-19 16:00:41 +0800

    提交2

--- lrcspider.py
+++ lrcspider.py
@@ -4,81 +4,89 @@ import urllib
 from HTMLParser import HTMLParser
 import sys
 import traceback
-
+abs
+a
 
 success = 0
+
+
 def changecode():
     try:
         new = 'utf-8'
         default = sys.getdefaultencoding()
-        print default,new
+        print(default, new)
         if new != default:
-            reload(sys)
-            print(default,new)
+            #reload(sys)
+            print(default, new)
             sys.setdefaultencoding(new)
-    except Exception,e:
-        print 'changecode except'
-        print Exception,":",e
-        #traceback.print_exc()
+    except Exception as e:
+        print('changecode except')
+        print(Exception, ":", e)
+        # traceback.print_exc()
     else:
-        print sys.getdefaultencoding()
+        print(sys.getdefaultencoding())
     finally:
-        print sys.getdefaultencoding()
-        
+        print(sys.getdefaultencoding())
+
+
 def spider(url):
     content = ''
     try:
         f = urllib.urlopen(url)
-        #print f
+        # print f
         content = f.read()
-        #print content
+        # print content
     except:
-        print 'spider exception:' + url, 
+        print
+        'spider exception:' + url,
     return content
- 
 
-class myHtmlParser(HTMLParser):  
-    #处理<!开头的内容  
-    #def handle_decl(self,decl):  
-        #print 'Encounter some declaration:'+ decl  
-    def handle_starttag(self,tag,attrs):  
-        if tag == 'a':    
+
+class myHtmlParser(HTMLParser):
+    # 处理<!开头的内容
+    # def handle_decl(self,decl):
+    # print 'Encounter some declaration:'+ decl
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
             for attr in attrs:
                 if len(attr) == 2 and attr[0] == 'class' and attr[1].startswith('down-lrc-btn'):
-                    #print "attr [%d]:%s" % (len(attr), attr)
+                    # print "attr [%d]:%s" % (len(attr), attr)
                     url = '''http://music.baidu.com''' + attr[1][23:-3]
-                    #print url
+                    # print url
                     idx = url.rfind('/')
                     if idx != -1:
-                        path = 'F:\\Python\\lrc\\' + url[idx+1:]
-                        print path
+                        path = 'F:\\Python\\lrc\\' + url[idx + 1:]
+                        print
+                        path
                         lrc = spider(url)
                         if len(lrc) > 0:
-                            f = file(path, 'w')   # open for 'w'riting
-                            f.write(lrc)               # write text to file
-                            f.close()                   # close the file
+                            f = open(path, 'w')  # open for 'w'riting
+                            f.write(lrc)  # write text to file
+                            f.close()  # close the file
 
-    #def handle_endtag(self,tag):  
-        #print 'Encounter the end of a %s tag' % tag  
-    #处理注释  
-    #def handle_comment(self,comment):   
-        #print 'Encounter some comments:' + comment
+    # def handle_endtag(self,tag):
+    # print 'Encounter the end of a %s tag' % tag
+    # 处理注释
+    # def handle_comment(self,comment):
+    # print 'Encounter some comments:' + comment
+
 
 if __name__ == '__main__':
     idx = 0
     while True:
         url = '''http://music.baidu.com/search/lrc?key=%s&start=%d&size=20&third_type=0''' % ('%E6%AD%8C%E8%AF%8D', idx)
-        print url 
+        print
+        url
         content = spider(url)
         if len(content) > 0:
             try:
                 htmlparser = myHtmlParser()
                 htmlparser.feed(content)
-            except Exception,e:
-                print Exception,":",e
-                #traceback.print_exc()
+            except Exception as e:
+                print(Exception, ":", e)
+                # traceback.print_exc()
         idx += 20
         if idx > 10000:
             break
-    
-    
+
+
